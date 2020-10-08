0f0a5db5832bbd2b82e486327879522af5fd115c
Alex House <alexhcreates@gmail.com>
2020-10-08 17:31:34 -0500

    Added page clicks by type

--- js/page-feed-web-connect.js
+++ js/page-feed-web-connect.js
@@ -15,6 +15,8 @@ var insightsMetrics = [
     'post_impressions_viral_unique',
     'post_clicks',
     'post_clicks_unique',
+    'post_clicks_by_type'
+    'post_clicks_by_type_unique'
     'post_engaged_users',
     'post_negative_feedback',
     'post_negative_feedback_unique',
@@ -274,6 +276,14 @@ myConnector.getSchema = function(schemaCallback) {
             dataType: tableau.dataTypeEnum.int
         }, {
             id: "post_clicks_unique",
+            alias: "Post Clicks Unique",
+            dataType: tableau.dataTypeEnum.int
+        }, {
+            id: "post_clicks_by_type",
+            alias: "Post Clicks Unique",
+            dataType: tableau.dataTypeEnum.int
+        },{
+            id: "post_clicks_by_type_unique",
             alias: "Post Clicks Unique",
             dataType: tableau.dataTypeEnum.int
         }, {
